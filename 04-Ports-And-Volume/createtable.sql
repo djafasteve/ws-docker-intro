f0a364d1b99915ba9d9af232db2c8a55856a5753
tarNeFyS <tarnefys@gmail.com>
2019-11-13 13:45:49 +0100

    Some rectif

--- 04-Ports-And-Volume/createtable.sql
+++ 04-Ports-And-Volume/createtable.sql
@@ -1,2 +1,2 @@
 CREATE TABLE IF NOT EXISTS trolls (identifier INT, value VARCHAR);
-INSERT INTO trolls VALUES (1, 'Jaybe'), (2, 'kevinkh')
+INSERT INTO trolls VALUES (1, 'Jaybe'), (2, 'kevinkh'), (3, 'Thomas Choucroute')
